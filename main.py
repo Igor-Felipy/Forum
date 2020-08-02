27eb75f780823a3ac9944bea966f4d926889408f
Igor-Felipy <igor.felipy123@gmail.com>
2020-08-02 18:24:05 -0400

    idealizando o esqueleto da aplicacao

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+
+app =  Flask(__name__)
+
+
+#just the main page
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/p/<int:post>')
+def postRender(post):
+
+    return str(post)
+# title
+# text
+# coments
+
+
+@app.route('/t/<topic>/', methods=['GET'])
+def topic(topic):
+
+    return topic
+# the lasts post on this topic
+
+
+if __name__=="__main__":
+    app.run(debug=True)
